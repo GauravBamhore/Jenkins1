0ff38526accfffc355056ca1ca3240fe93bce173
Gaurav B <gauravbamhore22@gmail.com>
2023-06-03 19:38:07 +0530

    commit no.1

---
+++ src/test/java/Jenkins/Jenkins_Study.java
@@ -0,0 +1,35 @@
+package Jenkins;
+
+import static org.testng.Assert.expectThrows;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Jenkins_Study {
+	@Test
+	public void TC_001() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\bamho\\eclipse-workspace\\JenkinPush\\Driver\\chromedriver.exe");
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--start-maximized");
+		option.addArguments("--remote-allow-origins=*");
+
+		// option.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(option);
+		driver.get("http://www.seleniumframework.com/Practiceform/");
+		WebElement alert = driver.findElement(By.xpath("//button[@id='alert']"));
+		alert.click();
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.alertIsPresent());
+		
+
+	}
+}
